6ac9f7a16c191f07a9a278a97bcff69a07030abf
Kazuya Arakawa <arakawa@4527.info>
2016-04-28 19:10:39 +0900

    N + 1 Problem

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,3 @@
 class Follow < ActiveRecord::Base
+  belongs_to :follow_user, class_name: 'User', foreign_key: :follow_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :follows, foreign_key: :user_id
+  has_many :follow_users, through: :follows, source: :follow_user
 end
